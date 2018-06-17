60a368de19a6bac1bc336d65559baff02214275c
paul antezana <contacto@paulantezana.com>
2018-06-16 20:38:42 -0500

    new updates

--- assets/main.js
+++ assets/main.js
@@ -1,22 +1,4 @@
-$(".openbtn").on("click", function() {
-    $(".ui.sidebar").toggleClass("very thin icon");
-    $(".asd").toggleClass("marginlefting");
-    $(".sidebar z").toggleClass("displaynone");
-    $(".ui.accordion").toggleClass("displaynone");
-    $(".ui.dropdown.item").toggleClass("displayblock");
- 
-    $(".logo").find('img').toggle();
- 
+$('.ui.sidebar').sidebar({
+    context: $('.bottom.segment')
   })
-  $(".ui.dropdown").dropdown({
-    allowCategorySelection: true,
-    transition: "fade up",
-    context: 'sidebar',
-    on: "hover"
-  });
- 
-  $('.ui.accordion').accordion({
-    selector: {
- 
-    }
-  });
+  .sidebar('attach events', '.menu .item');
